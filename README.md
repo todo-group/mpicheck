16f1cbe684045d19eb1b88d2949d7a4338ef41ff
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2024-11-04 10:21:47 +0900

    update gpucheck

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # mpicheck
+
+## mpicheck
+
+```bash
+$ cmake -B build .
+$ cmake --build build
+$ ./build/mpicheck
+```
+
+## gpucheck
+
+```bash
+$ ./venv .venv
+$ source .venv/bin/activate
+$ python3 gpucheck.py
+```
--- gpucheck.py
+++ gpucheck.py
@@ -3,10 +3,15 @@
 import os
 import torch
 
-# print(os.environ['CUDA_VISIBLE_DEVICES'])
-print(torch.cuda.is_available())
-print(torch.cuda.device_count())
-print(torch.cuda.current_device())
-print(torch.cuda.get_device_name())
-print(torch.cuda.get_device_capability())
-
+if torch.cuda.is_available():
+    print("CUDA is available.")
+    if os.environ.get('CUDA_VISIBLE_DEVICES') is None:
+        print("CUDA_VISIBLE_DEVICES: not defined")
+    else:
+        print(f"CUDA_VISIBLE_DEVICES: {os.environ['CUDA_VISIBLE_DEVICES']}")
+    print(f"Device Count: {torch.cuda.device_count()}")
+    print(f"Device Number: {torch.cuda.current_device()}")
+    print(f"Device Name: {torch.cuda.get_device_name()}")
+    print(f"Device Capability: {torch.cuda.get_device_capability()}")
+else:
+    print("CUDA is NOT available.")
